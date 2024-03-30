f9ec4020b8665bd1df03864b87806719ed38a331
Zhihare <vladislav.zhihar@gmail.com>
2024-03-30 22:55:02 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.json());
+  app.use(cors());
   await app.listen(3001);
 }
 bootstrap();
